673aeff185b236ad6a676cca839a985203c13c3b
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-02-24 23:51:52 +0000

    Add initial input

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace NumberGuesser
         static void Main(string[] args)
         {
             string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Lawrence";
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("$Hello{0}, let's play a game", input);
+
         }
     }
 }
